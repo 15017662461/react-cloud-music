56ea2d892d488f2af9e2cddaa1d09a749394582e
lqh <15017662461@163.com>
2021-09-06 19:41:55 +0800

    +

---
+++ src/api/request.js
@@ -0,0 +1,23 @@
+import { axiosInstance,categoryMap } from "./config";
+
+export const getBannerRequest = () => {
+  return axiosInstance.get ('/banner');
+}
+
+export const getRecommendListRequest = () => {
+  return axiosInstance.get ('/personalized');
+}
+
+export const getHotSingerListRequest = (count) => {
+  return axiosInstance.get(`/top/artists?offset=${count}`);
+}
+
+export const getSingerListRequest= (category, alpha, count) => {
+  const { type, area } = !!category ? categoryMap.get(category) : {}; 
+  return axiosInstance.get( `/artist/list?${ type && area ? `type=${type}&area=${area}` : '' }&initial=${alpha.toLowerCase()}&offset=${count}` );
+  // return axiosInstance.get(`/artist/list?cat=${category}&initial=${alpha.toLowerCase()}&offset=${count}`);
+}
+
+export const getRankListRequest = () => {
+  return axiosInstance.get (`/toplist/detail`);
+};
--- src/baseUI/scroll/index.js
+++ src/baseUI/scroll/index.js
@@ -1,13 +1,38 @@
-import { forwardRef, useEffect, useRef, useState, useImperativeHandle } from "react";
+import { forwardRef, useEffect, useRef, useState, useImperativeHandle, useMemo } from "react";
 import PropTypes from "prop-types"
 import BScroll from "better-scroll"
 import styled from 'styled-components';
+import LoadingV2 from '../loading-v2';
+import Loading from '../loading';
+import { debounce } from "../../api/utils";
 
 const ScrollContainer = styled.div`
   width: 100%;
   height: 100%;
   overflow: hidden;
 `
+
+const PullUpLoading = styled.div`
+  position: absolute;
+  left:0; right:0;
+  bottom: 5px;
+  width: 60px;
+  height: 60px;
+  margin: auto;
+  z-index: 100;
+`;
+
+export const PullDownLoading = styled.div`
+  position: absolute;
+  left:0; right:0;
+  top: 0px;
+  height: 30px;
+  margin: auto;
+  z-index: 100;
+`;
+
+
+
 
 const Scroll = forwardRef((props, ref) => {
   const [bScroll, setBScroll] = useState()
@@ -16,7 +41,17 @@ const Scroll = forwardRef((props, ref) => {
 
   const { direction, click, refresh, bounceTop, bounceBottom } = props
 
-  const { pullUp, pullDown, onScroll } = props;
+  const { pullUp, pullDown, onScroll, pullUpLoading, pullDownLoading } = props;
+
+  let pullUpDebounce = useMemo(() => {
+    return debounce(pullUp, 300)
+  }, [pullUp]);
+  // 千万注意，这里不能省略依赖，
+  // 不然拿到的始终是第一次 pullUp 函数的引用，相应的闭包作用域变量都是第一次的，产生闭包陷阱。下同。
+
+  let pullDownDebounce = useMemo(() => {
+    return debounce(pullDown, 300)
+  }, [pullDown]);
 
   useEffect(() => {
     const scroll = new BScroll(scrollContainerRef.current, {
@@ -33,6 +68,7 @@ const Scroll = forwardRef((props, ref) => {
     return () => {
       setBScroll(null)
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   useEffect(() => {
@@ -45,30 +81,36 @@ const Scroll = forwardRef((props, ref) => {
     }
   }, [onScroll, bScroll])
 
+  // 滑动到底部
   useEffect(() => {
     if (!bScroll || !pullUp) return;
-    bScroll.on('scrollEnd', () => {
+    const handlePullUp = () => {
+      //判断是否滑动到了底部
       if (bScroll.y <= bScroll.maxScrollY + 100) {
-        pullUp();
+        pullUpDebounce();
       }
-    })
+    };
+    bScroll.on('scrollEnd', handlePullUp);
+    // 解绑
     return () => {
-      bScroll.off('scrollEnd')
+      bScroll.off('scrollEnd', handlePullUp);
     }
-  }, [pullUp, bScroll])
+  }, [pullUp, pullUpDebounce, bScroll]);
 
+  // 判断用户的下拉动作
   useEffect(() => {
     if (!bScroll || !pullDown) return;
-    bScroll.on('touchEnd', (pos) => {
+    const handlePullDown = (pos) => {
       //判断用户的下拉动作
       if (pos.y > 50) {
-        pullDown();
+        pullDownDebounce();
       }
-    });
+    };
+    bScroll.on('touchEnd', handlePullDown);
     return () => {
-      bScroll.off('touchEnd');
+      bScroll.off('touchEnd', handlePullDown);
     }
-  }, [pullDown, bScroll]);
+  }, [pullDown, pullDownDebounce, bScroll]);
 
   useEffect(() => {
     if (refresh && bScroll) {
@@ -93,20 +135,24 @@ const Scroll = forwardRef((props, ref) => {
     }
   }));
 
-  return(
+  const PullUpdisplayStyle = pullUpLoading ? { display: "" } : { display: "none" };
+  const PullDowndisplayStyle = pullDownLoading ? { display: "" } : { display: "none" };
+  return (
     <ScrollContainer ref={scrollContainerRef}>
-      {
-        props.children
-      }
+      {props.children}
+      {/* 滑到底部加载动画 */}
+      <PullUpLoading style={PullUpdisplayStyle}><Loading></Loading></PullUpLoading>
+      {/* 顶部下拉刷新动画 */}
+      <PullDownLoading style={PullDowndisplayStyle}><LoadingV2></LoadingV2></PullDownLoading>
     </ScrollContainer>
-  )
+  );
 })
 
 Scroll.defaultProps = {
   direction: "vertical",
   click: true,
   refresh: true,
-  onScroll:null,
+  onScroll: null,
   pullUpLoading: false,
   pullDownLoading: false,
   pullUp: null,
@@ -116,7 +162,7 @@ Scroll.defaultProps = {
 }
 
 Scroll.propTypes = {
-  direction: PropTypes.oneOf (['vertical', 'horizental']),
+  direction: PropTypes.oneOf(['vertical', 'horizental']),
   refresh: PropTypes.bool,
   onScroll: PropTypes.func,
   pullUp: PropTypes.func,
